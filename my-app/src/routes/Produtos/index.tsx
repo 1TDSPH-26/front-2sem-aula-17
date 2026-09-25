d2c33838303f6a3a263056b2e8a5f3576550de03
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-09-25 09:38:23 -0300

    feat : GetAllProdutos e Delete, utilizando o fetch

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,23 +1,28 @@
 import { useEffect, useState } from "react";
 import type { TipoProduto } from "../../types/types";
+import { Link, useNavigate } from "react-router";
+import { FaRegEdit as Editar } from "react-icons/fa";
+import { MdOutlineDeleteForever as Excluir } from "react-icons/md";
 
 export default function Produtos() {
   document.title = "Produtos";
 
-  const[produtos,setProdutos] = useState<TipoProduto[]>([]);
+  const navigate = useNavigate()
 
-  useEffect( ()=>{
+  const [produtos, setProdutos] = useState<TipoProduto[]>([]);
 
-    const carregaProdutos = async ()=>{
+  useEffect(() => {
+
+    const carregaProdutos = async () => {
 
       try {
         const resposta = await fetch("http://localhost:3001/produtos");
-        
-        if(!resposta.ok){
+
+        if (!resposta.ok) {
           throw new Error("Erro na listagem dos produtos!");
         }
 
-        const data:TipoProduto[] = await resposta.json();
+        const data: TipoProduto[] = await resposta.json();
         setProdutos(data);
 
       } catch (error) {
@@ -27,40 +32,66 @@ export default function Produtos() {
 
     carregaProdutos();
 
-  },[]);
+  }, []);
+
+  const handleDelete = async (id: string) => {
+    try {
+
+      const resposta = await fetch(`http://localhost:3001/produtos/${id}`, {
+        method: "DELETE"
+      });
+
+      //ERROR
+      if(!resposta.ok){
+        throw new Error(`Ocorreu um erro na exclusão do produto: ${resposta.status} - ${resposta.statusText}`);
+      }
+
+      //SUCCESS
+      alert("Produto excluído com sucesso!");
+      //REDIRECT
+      navigate("/");
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <main>
-        <h2>Produtos</h2>
-        <div>
-          <table border={1} style={{borderCollapse:"collapse"}}>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>NOME</th>
-                <th>PREÇO</th>
-                <th>ESTOQUE</th>
-                <th>AÇÕES</th>
+      <h2>Produtos</h2>
+      <div>
+        <table border={1} style={{ borderCollapse: "collapse" }}>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>NOME</th>
+              <th>PREÇO</th>
+              <th>ESTOQUE</th>
+              <th>AÇÕES</th>
+            </tr>
+          </thead>
+          <tbody>
+            {produtos.map((produto) => (
+              <tr key={produto.id}>
+                <td>{produto.id}</td>
+                <td>{produto.nome}</td>
+                <td>{produto.preco}</td>
+                <td>{produto.estoque}</td>
+                <td>
+                  <Link to={`/editar-produtos/${produto.id}`}> <Editar /> </Link>
+                  |
+                  <Link to="#" onClick={() => handleDelete(produto.id)}><Excluir /></Link>
+                </td>
               </tr>
-            </thead>
-            <tbody>
-              {produtos.map( (produto)=>(
-                <tr key={produto.id}>
-                  <td>{produto.id}</td>
-                  <td>{produto.nome}</td>
-                  <td>{produto.preco}</td>
-                  <td>{produto.estoque}</td>
-                  <td>EDITAR/<button>Excluir</button></td>
-                </tr>
-              ))}
-            </tbody>
-            <tfoot>
-              <tr>
-                <td colSpan={5}>Quantidade de produtos: {produtos.length}</td>
-              </tr>
-            </tfoot>
-          </table>
-        </div>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan={5}>Quantidade de produtos: {produtos.length}</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
     </main>
   )
 }
