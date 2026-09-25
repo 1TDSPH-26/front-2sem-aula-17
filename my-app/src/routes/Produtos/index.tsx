e5d17aa8afafe3fc92cea5facfd8be1e421c5cbf
gabefeitosa <gabrielfeitosa321@outlook.com>
2026-09-25 09:46:35 -0300

    feat: métodos http post e delete  implementado

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,8 +1,14 @@
 import { useEffect, useState } from "react";
 import type { TipoProduto } from "../../types/types";
+import { Link, useNavigate } from "react-router";
+import { FaRegEdit as Editar } from "react-icons/fa";
+import { MdDeleteOutline as Excluir } from "react-icons/md";
+
 
 export default function Produtos() {
   document.title = "Produtos";
+
+  const navigate = useNavigate();
 
   const[produtos,setProdutos] = useState<TipoProduto[]>([]);
 
@@ -29,6 +35,24 @@ export default function Produtos() {
 
   },[]);
 
+  const handleDelete = async (id: string) =>{
+    try {
+      const resposta = await fetch(`http://localhost:3001/produtos/${id}`, {
+          method:"DELETE"
+      });
+
+      if (!resposta.ok){
+        throw new Error(`Ocourreu um erro na exclusão do produto: ${resposta.status} - ${resposta.statusText}`);
+      }
+
+      alert("Produto excluído com sucesso!");
+      navigate("/");
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <main>
         <h2>Produtos</h2>
@@ -50,7 +74,11 @@ export default function Produtos() {
                   <td>{produto.nome}</td>
                   <td>{produto.preco}</td>
                   <td>{produto.estoque}</td>
-                  <td>EDITAR/<button>Excluir</button></td>
+                  <td>
+                    <Link to={`/editar-produtos/${produto.id}`}> <Editar/> </Link>
+                    |
+                    <Link to="a" onClick={()=> handleDelete(produto.id)}> <Excluir/> </Link>
+                    </td>
                 </tr>
               ))}
             </tbody>
